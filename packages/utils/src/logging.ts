c277231c52528f4cffaf1eeb607d9ea635d47047
Theo Gravity <theo@switchboard.app>
2024-12-27 10:38:51 -0800

    fix: [AIR-1318] change log level to error only on prod

--- packages/utils/src/logging.ts
+++ packages/utils/src/logging.ts
@@ -1,10 +1,25 @@
-import { LogLayer, LoggerType } from "loglayer";
+import { LogLayer, LoggerType, type PluginShouldSendToLoggerParams } from "loglayer";
 
 const logger = new LogLayer({
   logger: {
     instance: console,
     type: LoggerType.CONSOLE,
   },
+  plugins: [
+    {
+      shouldSendToLogger(params: PluginShouldSendToLoggerParams) {
+        // This would be empty on the client side; client-side logging is fine
+        if (!process.env.NODE_ENV) {
+          return true;
+        }
+
+        // server-side
+        // Do not log anything in production if the log level is not error
+        // This is to avoid logging sensitive information in production
+        return !(process.env.NODE_ENV === "production" && params.logLevel !== "error");
+      },
+    },
+  ],
 });
 
 export function getLogger(): LogLayer {
