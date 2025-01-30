0f0ae36ad38c53619cc3239d78f5ed5e85cc7dc4
Cesar Sanchez <cesar.sanchezibr@gmail.com>
2025-01-30 16:09:00 -0300

    [Feat air-1646] Use profile names in "LinkedIn data extraction" example (#19)
    * feat(air-1646): use profile names in linkedin-data-extraction example
    
    * chore(air-1646): update sdk and remove dev environment

--- examples/linkedin-data-extraction/src/components/views/DisplayPromptResponse.tsx
+++ examples/linkedin-data-extraction/src/components/views/DisplayPromptResponse.tsx
@@ -2,11 +2,11 @@ import { Button, Card, CardContent, CardHeader, CardTitle } from "@local/ui";
 
 interface DisplayPromptResponseProps {
   content: string;
-  profileId?: string;
+  profileName?: string;
   tryAgain?: () => void;
 }
 
-export function DisplayPromptResponse({ content, profileId, tryAgain }: DisplayPromptResponseProps) {
+export function DisplayPromptResponse({ content, profileName, tryAgain }: DisplayPromptResponseProps) {
   return (
     <Card>
       <CardHeader className="flex flex-column items-start justify-between pb-2">
@@ -15,7 +15,7 @@ export function DisplayPromptResponse({ content, profileId, tryAgain }: DisplayP
             Try Again
           </Button>
         )}
-        <CardTitle>Profile ID: {profileId}</CardTitle>
+        <CardTitle>Profile Name: {profileName}</CardTitle>
         <CardTitle>Scraped Data</CardTitle>
       </CardHeader>
       <CardContent>
